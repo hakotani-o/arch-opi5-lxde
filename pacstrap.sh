56b715b0b4893f819011c0723c29d1a4ab254c10
hakotani-o <139629056+hakotani-o@users.noreply.github.com>
2026-07-10 23:28:43 +0900

    Update pacstrap.sh

--- pacstrap.sh
+++ pacstrap.sh
@@ -7,6 +7,7 @@ sudo pacman-key --init
 sudo cp  etc/pacman.d/mirrorlist /etc/pacman.d
 sudo cp  -a keyrings /usr/share/
 sudo cp  etc/pacman.d/mirrorlist /etc/pacman.d
+sudo cp etc/pacman.conf /etc
 sudo sudo pacman-key --populate archlinuxarm
 sudo pacman -Sy --noconfirm archlinuxarm-keyring
 sudo pacman -Syyu
