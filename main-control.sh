5eed4d0958e9ae4ac54e81889052d36c3abed9d7
hakotani-o <139629056+hakotani-o@users.noreply.github.com>
2026-07-12 09:03:18 +0900

    Update main-control.sh

--- main-control.sh
+++ main-control.sh
@@ -2,9 +2,9 @@
 set -eE
 
 #	sudo rm -rf ./mnt Arch-linux-aarch64-orangepi-5-2026*.img arch-linux.rootfs.tar.gz
-	sudo ./disk_image.sh orangepi-5 rk3588s-orangepi-5
+	./das-u-boot.sh orangepi-5-rk3588s_defconfig
 	./pacstrap.sh 
 	sudo ./lxdm-setup.sh
-	./das-u-boot.sh orangepi-5-rk3588s_defconfig
+	sudo ./disk_image.sh orangepi-5 rk3588s-orangepi-5
 	./das-u-boot.sh orangepi-5-plus-rk3588_defconfig
 	sudo ./disk_image.sh orangepi-5-plus rk3588-orangepi-5-plus
